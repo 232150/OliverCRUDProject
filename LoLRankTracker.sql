7df88c13c0c9d1ddb42cabbef3f95ac269e182fd
232150 <35493733+232150@users.noreply.github.com>
2018-05-29 07:53:47 -0500

    Add files via upload

---
+++ LoLRankTracker.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 29, 2018 at 12:50 PM
+-- Server version: 5.5.57-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `LoLRankTracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rank`
+--
+
+CREATE TABLE IF NOT EXISTS `Rank` (
+  `Username` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `LP` int(11) NOT NULL,
+  `Division` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `Username` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
